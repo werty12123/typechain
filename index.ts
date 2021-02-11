fa9b133d346a662c1fe63d51aa457ecc307e6633
werty12123 <68688518+werty12123@users.noreply.github.com>
2021-02-12 00:10:26 +0900

    typescript

---
+++ index.ts
@@ -0,0 +1,9 @@
+const nam = "Nicolas", age = 24, gender = "male";
+
+const sayHi = (nam, age, gender) => {
+    console.log(`Hello ${nam}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(nam, age, gender);
+
+export{};
