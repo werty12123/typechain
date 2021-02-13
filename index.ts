b4ee418a8a65fd323fe68591290905000fed8ae2
werty12123 <68688518+werty12123@users.noreply.github.com>
2021-02-14 01:29:12 +0900

    blockchain

--- index.ts
+++
@@ -1,9 +0,0 @@
-const nam = "Nicolas", age = 24, gender = "male";
-
-const sayHi = (nam, age, gender) => {
-    console.log(`Hello ${nam}, you are ${age}, you are a ${gender}`);
-};
-
-sayHi(nam, age, gender);
-
-export{};
